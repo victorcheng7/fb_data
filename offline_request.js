6f7e9d83971b22ace9e40b47b2f6bfd90f6fc472
victorcheng7 <victor_cheng@yahoo.com>
2018-12-13 01:55:01 -0800

    finished bestfriends

--- offline_request.js
+++ offline_request.js
@@ -12,20 +12,19 @@ fs.readFile('data/messageData.txt', function(err, data) {
 });*/
 
 fs.readFile('data/large_messageData.txt', function(err, data) {
-	// TODO check if it's a group (who are the participants in the group)
+	// TODO check if it's a group (who are the participants in the group). thread.isGroup
+
 	const messageList = JSON.parse(data);
-	//console.log(Math.max.apply(null, helper.flattenArrayByKey(messageList, "messageCount")));
 	// Sorted by # of messages
 	const sortedMessageList = messageList.concat().sort(function(a, b){return b.messageCount-a.messageCount});
 	const dateOneYearAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-	//console.log(sortedMessageList[0].messageCount);
 	// TODO think about what happens in groups
 	function findOldBestFriends() { // Returns old best friends
 		var bestFriends = [];
 		var i = 0;
 		while(bestFriends.length < 3 && i < sortedMessageList.length) {
 			var friend = sortedMessageList[i];
-			if(sortedMessageList[i].lastMessageTimestamp < dateOneYearAgo && !friend.isGroup) bestFriends.push(sortedMessageList[i]);  
+			if(sortedMessageList[i].lastMessageTimestamp < dateOneYearAgo && !friend.isGroup && friend.muteUntil != null) bestFriends.push(sortedMessageList[i]);  
 			i++;
 		}
 		return[bestFriends[0], bestFriends[1], bestFriends[2]];
@@ -36,7 +35,7 @@ fs.readFile('data/large_messageData.txt', function(err, data) {
 		var i = 0;
 		while(bestFriends.length < 3 && i < sortedMessageList.length) {
 			var friend = sortedMessageList[i];
-			if(friend.lastMessageTimestamp >= dateOneYearAgo && !friend.isGroup) bestFriends.push(friend); 
+			if(friend.lastMessageTimestamp >= dateOneYearAgo && !friend.isGroup && friend.muteUntil != null) bestFriends.push(friend); 
 			i++; 
 		}
 		return[bestFriends[0], bestFriends[1], bestFriends[2]];
@@ -50,10 +49,9 @@ fs.readFile('data/large_messageData.txt', function(err, data) {
 	findRecentBestFriends().forEach(friend => console.log(friend.name));
 	console.log("Old best friends")
 	findOldBestFriends().forEach(friend => console.log(friend.name));
-	//console.log(messageList.find(thread => thread.isGroup));
-   	console.log(messageList[0]);
+   	// console.log(messageList[0]);
 });
-/*
+
 
 // Using api.getfriendList
 
@@ -81,4 +79,4 @@ fs.readFile('data/friendsData.txt', function(err, data) {
 	console.log("Most common female name: " + commonFemaleName[0] + " with count " + commonFemaleName[1]);
 	console.log("Most common male name: " + commonMaleName[0] + " with count " + commonFemaleName[1]);
 
-});*/
+});
